e6f8236ea6ff89f764c868ddd0de9691e34a25c3
Josh Beale <145503754+Requiem-theDM@users.noreply.github.com>
2024-09-27 15:34:49 -0400

    Improved ex3.py to handle any amount of metadata lines before the header.

--- week3/ex3.py
+++ week3/ex3.py
@@ -2,44 +2,26 @@
 
 #Establishes environment
 import sys
-
 import numpy
-
 import re
-
-vcf_raw = open(sys.argv[1])
 
 allele_freq = [] #Initialize a list to hold allele frequencies
 depth_dist = [] #Initializes a list to hold
 
-#Skips the first 77 lines of metadata
-for i in range(77): 
-    vcf_raw.readline() 
-
-header = vcf_raw.readline().rstrip("\n").split("\t") #Pulls the header from the 78th line of metadata
-info_pos = header.index("INFO") #Locates the column with info based off of its column in the header
-format_pos = header.index("FORMAT") #Locates the column with format based off of its column in the header
-
-#Locate the column with info for each member of the A01 series and store it into a list
-A01_series = [header.index("A01_09"),
-              header.index("A01_11"),
-              header.index("A01_23"),
-              header.index("A01_24"),
-              header.index("A01_27"),
-              header.index("A01_31"),
-              header.index("A01_35"),
-              header.index("A01_39"),
-              header.index("A01_62"),
-              header.index("A01_63")
-]
-
 #Reads the data of the vcf file line by line
-for line in vcf_raw:
-    fields = line.rstrip('\n').split('\t')
-    info = fields[info_pos]
-    allele_freq.append(info.split(";")[info.split(";").index([i for i in info.split(";") if re.findall(r'^AF=',i)][0])]) #Splits the info field at semicolons, locates allele frequency based off of its designator, then appends it to the list of frequencies.
-    for column in A01_series: #Scans through each column of the samples
-        depth_dist.append(fields[column].split(":")[fields[format_pos].split(":").index("DP")]) #Uses the format column to determine the index to pull read depth from a given sample
+for line in open(sys.argv[1]):
+    if line.startswith('##'):
+        continue #Skips all metadata lines except the header
+    if line.startswith('#'):
+        header = line.rstrip("\n").split("\t") #Pulls the header from the last line of metadata
+        info_pos = header.index("INFO") #Locates the column with info based off of its column in the header
+        format_pos = header.index("FORMAT") #Locates the column with format data based off of its column in the header
+        sample_pos = list(range(len(header[:format_pos + 1]),len(header))) #Locate the column with info for each sample and store it into a list
+    else:
+        fields = line.rstrip('\n').split('\t')
+        allele_freq.append(fields[info_pos].split(";")[fields[info_pos].split(";").index([i for i in fields[info_pos].split(";") if re.findall(r'^AF=',i)][0])]) #Pulls the info field based on the header, splits the info field at semicolons, locates allele frequency based off of its designator, then appends it to the list of frequencies.
+        for sample in sample_pos: #Scans through each column of the samples
+            depth_dist.append(fields[sample].split(":")[fields[format_pos].split(":").index("DP")]) #Uses the format column to determine the index to pull read depth from a given sample
 
 with open(f"AF.txt", "w") as file:
     file.write("Allele_Frequency\n")
@@ -50,5 +32,3 @@ with open(f"DP.txt", "w") as file:
     file.write("Read_Depth\n")
     for depth in depth_dist:
         file.write(f"{depth}\n")
-
-vcf_raw.close()
